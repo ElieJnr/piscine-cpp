1081734897a4b890815726ac2d2a0c3144269111
emalo <elie.jean.malo@gmail.com>
2025-05-02 12:05:44 +0000

    Improve comments for ultimatedivmod function for better clarity

--- ultimatedivmod.cpp
+++ ultimatedivmod.cpp
@@ -1,6 +1,10 @@
+// This function will divide the int a and b.
+// The result of this division will be stored in the int pointed by div.
+// The remainder of this division will be stored in the int pointed by mod.
+
 #include <iostream>
 
-void ultimatedivmod(int* a, int* b)
+void ultimatedivmod(int *a, int *b)
 {
     int temp = *a / *b;
     int temp00 = *a % *b;
